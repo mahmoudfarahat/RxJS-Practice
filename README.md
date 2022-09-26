57be913ad6b5d21760d4cf3ba46fb49db71ff8c0
Mahmoud Farahat <Mahmoud.Farahat@corp.arab-computers.com>
2022-09-26 13:38:48 +0200

    first commit

--- README.md
+++ README.md
@@ -16,3 +16,30 @@ const obs1$ = of('a','b','c').pipe(delay(3000));
     err => console.log('err is', err),
     () => console.log('complete')
   )
+
+
+  2- megre Map , flatMap
+  megre two observables with nested loop / loop on first observable then an equation on the sceond observable then flat all and return one observable 
+ 
+const letters$ = of ('a','c','b')
+const numbers$ = of (1,2,3)
+
+const combined = letters$.pipe(
+  mergeMap(letter => {
+    return  numbers$.pipe(map(numberEl => letter + numberEl))
+  })
+)
+combined.subscribe(console.log)
+
+3-swtichMap 
+swtich between observables and use the latest observable and unsubscribe the old
+ 
+  const click =fromEvent(document,'click')
+  click.pipe(
+   switchMap(() => {
+     return interval(1000)
+    })
+  )
+  .subscribe(x  => {
+   console.log(x)
+  })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { concat, delay, of, tap } from 'rxjs';
+import { concat, delay, fromEvent, interval, map, mergeMap, of, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,18 +12,45 @@ export class AppComponent implements OnInit{
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    const obs1$ = of('a','b','c').pipe(delay(3000));
-    const obs2$ = of('1','2','3').pipe(
-      tap(()=>{
-        throw new Error('Something went wrong')
-      })
-    );
-    const concatData = concat(obs1$,obs2$,obs1$)
-  concatData.subscribe(
-    value => console.log('value is', value),
-    err => console.log('err is', err),
-    () => console.log('complete')
-  )
+
+//3-swichMap 
+// const click =fromEvent(document,'click')
+// click.pipe(
+//   switchMap(() => {
+//     return interval(1000)
+//   })
+// )
+// .subscribe(x  => {
+//   console.log(x)
+// })
+
+
+  //2- megre Map , flatMap
+// const letters$ = of ('a','c','b')
+// const numbers$ = of (1,2,3)
+
+// const combined = letters$.pipe(
+//   mergeMap(letter => {
+//     return  numbers$.pipe(map(numberEl => letter + numberEl))
+//   })
+// )
+// combined.subscribe(console.log)
+
+
+   // 1- concat
+  //   const obs1$ = of('a','b','c').pipe(delay(3000));
+  //   const obs2$ = of('1','2','3').pipe(
+  //     tap(()=>{
+  //       throw new Error('Something went wrong')
+  //     })
+  //   );
+  //   const concatData = concat(obs1$,obs2$,obs1$)
+  // concatData.subscribe(
+  //   value => console.log('value is', value),
+  //   err => console.log('err is', err),
+  //   () => console.log('complete')
+    
+  // )
   }
 
 
